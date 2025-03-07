d259994d232eff288760053a9f7bd6cfe2df71b8
Allen Xiao <xhui.xiao@gmail.com>
2025-03-07 14:01:21 +0800

    Create Atrace.h

---
+++ app/src/main/cpp/include/Atrace.h
@@ -0,0 +1,19 @@
+#ifndef SHADERTOYANDROID_ATRACE_H
+#define SHADERTOYANDROID_ATRACE_H
+#include <android/trace.h>
+
+#define ATRACE_NAME(name) ScopedTrace ___tracer(name)
+#define ATRACE_CALL() ATRACE_NAME(__FUNCTION__)
+
+class ScopedTrace {
+public:
+    inline ScopedTrace(const char *name) {
+        ATrace_beginSection(name);
+    }
+
+    inline ~ScopedTrace() {
+        ATrace_endSection();
+    }
+};
+
+#endif //SHADERTOYANDROID_ATRACE_H

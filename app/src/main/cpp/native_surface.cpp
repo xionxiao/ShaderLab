49d51bd4d971a59185d577dc85ac03630635db28
Allen Xiao <xhui.xiao@gmail.com>
2025-10-28 22:53:53 +0800

    add JNI_OnLoad manually

--- app/src/main/cpp/native_surface.cpp
+++ app/src/main/cpp/native_surface.cpp
@@ -1,19 +1,19 @@
-#include <jni.h>
-#include <android/native_window_jni.h>
-#include <memory>
-#include <pthread.h>
 #include "ALog.h"
 #include "RenderThread.h"
 #include "Renderer.h"
+#include <android/native_window_jni.h>
+#include <jni.h>
+#include <memory>
+#include <pthread.h>
 
 #ifdef LOG_TAG
 #undef LOG_TAG
 #endif
-
 #define LOG_TAG "NativeSurfaceJNI"
+
 class NativeSurfaceView {
 public:
-    NativeSurfaceView(RenderType type, ANativeWindow* window) : mType(type) {
+    NativeSurfaceView(RenderType type, ANativeWindow *window) : mType(type) {
         mRenderer = Renderer::create(mType, window);
         mThread.setRenderer(mRenderer);
     }
@@ -21,60 +21,75 @@ public:
     void stop() { mThread.stop(); }
     void pause() { mThread.pause(); }
     void resume() { mThread.resume(); }
+
+public:
+    // Static JNI functions
+    static jlong onSurfaceCreated(JNIEnv *env, jobject thiz, jobject surface) {
+        ALOGD("%s", __func__);
+        auto window = ANativeWindow_fromSurface(env, surface);
+        auto renderer = new NativeSurfaceView(RenderType::GLES, window);
+        return (jlong)renderer;
+    }
+
+    static void onSurfaceChanged(JNIEnv *env, jobject thiz, jlong renderer,
+                                 jobject surface, jint format, jint width,
+                                 jint height) {
+        ALOGD("%s", __func__);
+        auto native_view = (NativeSurfaceView *)(renderer);
+        native_view->start();
+    }
+
+    static void onSurfaceDestroyed(JNIEnv *env, jobject thiz, jlong renderer,
+                                   jobject surface) {
+        ALOGD("%s", __func__);
+        auto native_renderer = (NativeSurfaceView *)(renderer);
+        delete native_renderer;
+    }
+
+    static void onSurfaceRedrawNeeded(JNIEnv *env, jobject thiz, jlong renderer,
+                                      jobject surface) {
+        ALOGD("%s", __func__);
+        // auto native_renderer = (NativeSurfaceView*)(renderer);
+        // native_renderer->start();
+    }
+
 private:
     RenderType mType;
     RenderThread mThread;
     std::shared_ptr<Renderer> mRenderer;
 };
 
-extern "C"
-JNIEXPORT jlong JNICALL
-Java_com_hweex_shadertoyandroid_ui_view_NativeSurfaceView_onSurfaceCreatedNative(JNIEnv *env,
-                                                                                 jobject thiz,
-                                                                                 jobject surface) {
-    // TODO: implement onSurfaceCreatedNative()
-    ALOGD("%s", __func__ );
-    auto window = ANativeWindow_fromSurface(env, surface);
-    auto renderer = new NativeSurfaceView(RenderType::GLES, window);
-    return (jlong) renderer;
-}
+#define JNI_CLASS_NAME "com/hweex/shadertoyandroid/ui/view/NativeSurfaceView"
 
-extern "C"
-JNIEXPORT void JNICALL
-Java_com_hweex_shadertoyandroid_ui_view_NativeSurfaceView_onSurfaceChangedNative(JNIEnv *env,
-                                                                                 jobject thiz,
-                                                                                 jlong renderer,
-                                                                                 jobject surface,
-                                                                                 jint format,
-                                                                                 jint width,
-                                                                                 jint height) {
-    // TODO: implement onSurfaceChangedNative()
-    ALOGD("%s", __func__ );
-    auto native_view= (NativeSurfaceView*)(renderer);
-    native_view->start();
+// JNI method table
+static JNINativeMethod gMethods[] = {
+    {"onSurfaceCreatedNative", "(Landroid/view/Surface;)J",
+     reinterpret_cast<void *>(NativeSurfaceView::onSurfaceCreated)},
+    {"onSurfaceChangedNative", "(JLandroid/view/Surface;III)V",
+     reinterpret_cast<void *>(NativeSurfaceView::onSurfaceChanged)},
+    {"onSurfaceDestroyedNative", "(JLandroid/view/Surface;)V",
+     reinterpret_cast<void *>(NativeSurfaceView::onSurfaceDestroyed)},
+    {"onSurfaceRedrawNeededNative", "(JLandroid/view/Surface;)V",
+     reinterpret_cast<void *>(NativeSurfaceView::onSurfaceRedrawNeeded)}};
 
-}
+// JNI registration function
+extern "C" JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *reserved) {
+    JNIEnv *env;
+    if (vm->GetEnv(reinterpret_cast<void **>(&env), JNI_VERSION_1_6) !=
+        JNI_OK) {
+        return JNI_ERR;
+    }
 
-extern "C"
-JNIEXPORT void JNICALL
-Java_com_hweex_shadertoyandroid_ui_view_NativeSurfaceView_onSurfaceDestroyedNative(JNIEnv *env,
-                                                                                   jobject thiz,
-                                                                                   jlong renderer,
-                                                                                   jobject surface) {
-    // TODO: implement onSurfaceDestroyedNative()
-    ALOGD("%s", __func__ );
-    auto native_renderer = (NativeSurfaceView*)(renderer);
-    delete native_renderer;
-}
+    jclass clazz = env->FindClass(JNI_CLASS_NAME);
+    if (clazz == nullptr) {
+        return JNI_ERR;
+    }
 
-extern "C"
-JNIEXPORT void JNICALL
-Java_com_hweex_shadertoyandroid_ui_view_NativeSurfaceView_onSurfaceRedrawNeededNative(JNIEnv *env,
-                                                                                      jobject thiz,
-                                                                                      jlong renderer,
-                                                                                      jobject surface) {
-    // TODO: implement onSurfaceRedrawNeededNative()
-    ALOGD("%s", __func__ );
-    // auto native_renderer = (NativeSurfaceView*)(renderer);
-    // native_renderer->start();
+    if (env->RegisterNatives(clazz, gMethods,
+                             sizeof(gMethods) / sizeof(gMethods[0])) < 0) {
+        return JNI_ERR;
+    }
+
+    ALOGD("%s", "JNI registration successful");
+    return JNI_VERSION_1_6;
 }
